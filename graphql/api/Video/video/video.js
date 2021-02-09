3708c0124cb8838bd06ad9c7337495159b793dc9
HJ <user@users-MacBook-Pro.local>
2021-02-10 02:32:03 +0900

    update video complete

--- graphql/api/Video/video/video.js
+++ graphql/api/Video/video/video.js
@@ -12,6 +12,19 @@ export default {
         return [];
       }
     },
+
+    getVideoOne: async (_, args) => {
+      const { id } = args;
+
+      try {
+        const result = await Video.findOne({ _id: id });
+
+        return result;
+      } catch (e) {
+        console.log(e);
+        return {};
+      }
+    },
   },
   Mutation: {
     registerVideo: async (_, args) => {
@@ -24,6 +37,37 @@ export default {
           description: desc,
         });
 
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+    deleteVideo: async (_, args) => {
+      const { id } = args;
+
+      try {
+        await Video.deleteOne({ _id: id });
+
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+    updateVideoOne: async (_, args) => {
+      const { id, path, title, desc } = args;
+      try {
+        await Video.updateOne(
+          { _id: id },
+          {
+            $set: {
+              thumbnailPath: path,
+              title,
+              description: desc,
+            },
+          }
+        );
         return true;
       } catch (e) {
         console.log(e);
